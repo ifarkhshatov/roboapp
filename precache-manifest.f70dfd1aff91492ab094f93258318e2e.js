cba597e016675dd8f05101cf079251f922adad16
Ildar Farkhshatov <ildar.farkhshatov@creamfinance.com>
2020-04-05 14:05:03 +0300

    Updates

---
+++ precache-manifest.f70dfd1aff91492ab094f93258318e2e.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1be1e697e39642fd5c80998803d0025c",
+    "url": "/roboapp/index.html"
+  },
+  {
+    "revision": "b628433163b8d524bb79",
+    "url": "/roboapp/static/css/2.3b6f1bda.chunk.css"
+  },
+  {
+    "revision": "e73e9717b72d14926b98",
+    "url": "/roboapp/static/css/main.983ed3d7.chunk.css"
+  },
+  {
+    "revision": "b628433163b8d524bb79",
+    "url": "/roboapp/static/js/2.79e767d9.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/roboapp/static/js/2.79e767d9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e73e9717b72d14926b98",
+    "url": "/roboapp/static/js/main.cf339381.chunk.js"
+  },
+  {
+    "revision": "51382b5f39b083f3bfa1",
+    "url": "/roboapp/static/js/runtime-main.ee43dd3c.js"
+  },
+  {
+    "revision": "afa3a1a65dd6239528bdd389d6196886",
+    "url": "/roboapp/static/media/HONEJS__.afa3a1a6.woff"
+  }
+]);
